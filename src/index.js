283963d6132ceb985ea7bd370c6b5744f07b453c
Jake Tran <jake.tranehs@gmail.com>
2019-07-05 03:33:16 -0700

    schema updated

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ require('dotenv').config()
 const { prisma } = require('../prisma/generated/prisma-client')
 const { GraphQLServer } = require('graphql-yoga')
 
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Appointment = require('./resolvers/Appointment')
+
 const resolvers = {
+    Query,
+    Mutation,
+    User,
+    Appointment
 }
 
 const server = new GraphQLServer({
